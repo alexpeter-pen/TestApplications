27aca253411568705168d230bde03695f5f12245
alexpeter-pen <118837759+alexpeter-pen@users.noreply.github.com>
2025-11-07 23:24:30 +0100

    Add files via upload

---
+++ Maturski Zadaci Web programiranje/4ЕИТ/4ЕИТ-Б18/Rešenje/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using B18_Matura.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace B18_Matura.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            VremenskaPrognoza prognoze = ExtractPrognoze();
+
+            return View(prognoze.Prognoze);
+        }
+
+        private VremenskaPrognoza ExtractPrognoze()
+        {
+            string path = Path.Combine(Server.MapPath("~/App_Data"), "VremenskaPrognoza.xml");
+
+            FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            XmlReader reader = new XmlTextReader(stream);
+
+            XmlSerializer deserializer = new XmlSerializer(typeof(VremenskaPrognoza));
+            var prognoze = (VremenskaPrognoza)deserializer.Deserialize(reader);
+            return prognoze;
+        }
+
+        public ActionResult Prognoza(string nazivMesta)
+        {
+            var prognoze = ExtractPrognoze();
+            var gradovi = prognoze.Prognoze.Select(sel => sel.NazivMesta).Distinct();
+
+            if (string.IsNullOrEmpty(nazivMesta))
+                nazivMesta = gradovi.First();
+
+            ViewBag.Cities = new SelectList(gradovi, nazivMesta);
+
+            return View(prognoze.Prognoze.FirstOrDefault(c => c.NazivMesta == nazivMesta));
+        }
+
+        public ActionResult Uputstvo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Maturski Zadaci Web programiranje/4ЕИТ/4ЕИТ-Б18/Rešenje/Models/Prognoza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace B18_Matura.Models
+{
+    public class Prognoza
+	{
+		public string Mesto { get; set; }
+		public string NazivMesta { get; set; }
+		public string MinTemperatura { get; set; }
+		public string MaxTemperatura { get; set; }
+		public string Vreme { get; set; }
+	}
+}
